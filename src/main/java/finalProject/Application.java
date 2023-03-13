62309822e815d02030938ff4056b8f48658d1199
pratheektek <pratheekp10@gmail.com>
2023-03-13 20:03:51 +0530

    java commit

---
+++ src/main/java/finalProject/Application.java
@@ -0,0 +1,14 @@
+package finalProject;
+
+import finalProject.screens.WelcomeScreen;
+
+public class Application {
+
+    public static void main(String[] args) {
+    	
+    	WelcomeScreen welcome = new WelcomeScreen();
+    	welcome.introWS();
+    	welcome.GetUserInput();
+
+    }
+}
---
+++ src/main/java/finalProject/services/ScreenService.java
@@ -0,0 +1,34 @@
+package finalProject.services;
+
+import finalProject.entities.Directory;
+import finalProject.screens.FileOptionsScreen;
+import finalProject.screens.Screen;
+import finalProject.screens.WelcomeScreen;
+
+
+public class ScreenService {
+	
+	//static Directory dir = new Directory();
+	
+	
+	
+	public static WelcomeScreen WelcomeScreen = new WelcomeScreen();
+    public static FileOptionsScreen FileOptionsScreen = new FileOptionsScreen();
+    
+    
+
+    public static Screen CurrentScreen = WelcomeScreen;
+
+    
+    public static Screen getCurrentScreen() {
+        return CurrentScreen;
+    }
+
+    
+    public static void setCurrentScreen(Screen currentScreen) {
+        CurrentScreen = currentScreen;
+    }
+    
+    
+    
+}
